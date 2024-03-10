5444159a0b62ed2c140091e389cac8e15ad4520d
Rahul <rahulvish194002@gmail.com>
2024-03-10 20:06:35 +0530

    Adding dynamic rendering of data

--- src/components/Foreground.jsx
+++ src/components/Foreground.jsx
@@ -5,14 +5,36 @@ function Foreground () {
     const data=[{
    
         desc:"React is the library for web and native user interfaces. Build user interfaces out of individual pieces called components written in JavaScript.",
-        fileSize:".9mb",
-        Download:"",
-        progress:""
+        fileSize:".09mb",
+        close:false,
+        tag:{
+            isOpen:false, tagTitle:"Download Now" , tagColor:"Green"
+        },
     
-      }]
+      },
+{
+   
+        desc:"React is the library for web and native user interfaces. Build user interfaces out of individual pieces called components written in JavaScript.",
+        fileSize:".09mb",
+        close:true,
+        tag:{
+            isOpen:true, tagTitle:"Download Now" , tagColor:"Green"
+        },
+    
+      },
+      {
+      desc:"React is the library for web and native user interfaces. Build user interfaces out of individual pieces called components written in JavaScript.",
+      fileSize:".09mb",
+      close:true,
+      tag:{
+          isOpen:true, tagTitle:"Download Now" , tagColor:"Green"
+      },
+  
+    },
+    ];
 
   return (
-    <div className='fixed top-0 z-[3] left-0 w-full h-full'>
+    <div className='fixed top-0 z-[3] left-0 w-full h-full flex gap-10 m-10  '>
         {data.map((item,index)=>(
             <Card data={item}/>
         ))}
